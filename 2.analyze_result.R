7d9a1ebcf255594094574cad798ec3decc0b5529
Jihoon Min <grcosmos@gmail.com>
2026-02-24 09:18:23 +0100

    Simplify some lines + Add more comments + Remove livestocks from nutriend calc. + Move footprinting scripts to analysis

--- 2.analyze_result.R
+++ 2.analyze_result.R
@@ -1,5 +1,145 @@
 #### Result analysis ####
 
+
+#### 1. Energy and Labor footprint ####
+
+# Total consumption-based footprint per country (23001 x 187)
+# By taking Diagonal(), we preserve the origin information in the intensity vectors. Target info is lost.
+# which can be good enough.
+
+fp_food <- lapply(l_int_d, function(d) Matrix::Diagonal(x=d) %*% FABIO_x_hh)
+
+energy_fp = Matrix::Diagonal(x=FABIO_en_int_d) %*% FABIO_x_hh #TJ = EJ/10^6 (sum = 22.5 EJ)
+# Note: "In the United States, food production uses 10.11 quadrillion Btu annually" = 10.7 EJ
+hr_m_fp = Matrix::Diagonal(x=FABIO_hr_m_int_d) %*% FABIO_x_hh #M.hr
+hr_f_fp = Matrix::Diagonal(x=FABIO_hr_f_int_d) %*% FABIO_x_hh #M.hr 
+
+colnames(energy_fp) = colnames(hr_m_fp) = colnames(hr_f_fp) = regions$iso3c
+
+# Country-wise consumption-based footprint 
+library(gt)
+
+l_int = readRDS(file = paste0("data/FABIO_exio_satellites_", year, ".rds"))
+
+consumption = "food"
+country = "KOR"
+extension = "hr_m_int_d" #"en_int_d", "hr_f_int_d"
+
+for (country in regions$iso3c) {
+  
+  for (extension in names(l_int)) {
+    print(paste("Calculating", extension, "footprint for", country))
+    
+    Y_country <- FABIO_y[, which(fd$iso3c == country)]
+    # Y_country <- Yi[, fd$iso3c == country]
+    colnames(Y_country) <- fd$fd[fd$iso3c == country]
+    
+    pop = subset(countrypops, country_code_3 == country & year == yr)$population
+    if (country %in% setdiff(regions$iso3c, unique(countrypops$country_code_3))) {pop=NA}
+    
+    print(paste("Population =", pop))
+    
+    # if(spread_stocks){
+    #   stock_ratio <- Y_country[, "stock_addition"] / (rowSums(Y_country) - Y_country[, "stock_addition"])
+    #   stock_ratio[!is.finite(stock_ratio)] <- 0
+    #   Y_country <- as.data.table(as.matrix(Y_country))
+    #   Y_country[, `:=`(food = food * (1 + stock_ratio),
+    #                    other = other * (1 + stock_ratio),
+    #                    tourist = tourist * (1 + stock_ratio),
+    #                    unspecified = unspecified * (1 + stock_ratio),
+    #                    stock_addition = 0)]
+    # }
+    
+    int <- l_int[[extension]]
+    MP <- int * FABIO_L
+    
+    # Initialize empty matrix to store results (row: exporter, col: importer)
+    mat <- matrix(0, nrow=nrreg, ncol = nrreg, byrow=TRUE)
+    rownames(mat) <- colnames(mat) <- sort(regions$iso3c)
+    
+    # Calculate footprints (energy & labor hr)
+    country_consump = as.vector(as.matrix(Y_country[, consumption]))
+    FP <- t(t(MP) * country_consump) # <= dim (23001x23001)
+    colnames(FP) <- rownames(FP) <- paste0(io$iso3c, "_", io$item)
+    FP <- as(FP, "TsparseMatrix")
+    
+    # Calculate calorie & protein production/consumption 
+    x_country = t(t(FABIO_L) * country_consump) # mass flows
+    # FP_cal = sweep(x_country, 2, 1000*coeff_cal, "*") 
+    FP_cal = t(t(x_country) * as.vector(coeff_cal$kcal_per_kg)) * 1000 # kcal flows
+    FP_pro = t(t(x_country) * as.vector(coeff_pro$protein_per_kg)) * 1000 # g-protein flows
+    colnames(FP_cal) <- rownames(FP_cal) <- colnames(FP_pro) <- rownames(FP_pro) <- paste0(io$iso3c, "_", io$item)
+    FP_cal <- as(FP_cal, "TsparseMatrix")
+    FP_pro <- as(FP_pro, "TsparseMatrix")
+    
+    cal_consum = sum(country_consump*coeff_cal$kcal_per_kg)/365/pop*1000 #kcal/cap/day
+    pro_consum = sum(country_consump*coeff_pro$protein_per_kg)/365/pop*1000 #kcal/cap/day
+    
+    results <- data.table(origin=rownames(FP)[FP@i + 1], target=colnames(FP)[FP@j + 1], 
+                          value = FP@x, # M.hr
+                          value_pcap = FP@x / pop * 1e6, # hr/cap (for labor)
+                          cal = FP_cal@x,
+                          cal_pcap = FP_cal@x / pop, # kcal per capita
+                          pro = FP_pro@x,
+                          pro_pcap = FP_pro@x / pop  # g-protein per capita
+    ) 
+    results[,`:=`(country_consumer = country,
+                  year = year,
+                  indicator = extension,
+                  country_origin = substr(origin,1,3),
+                  item_origin = substr(origin,5,100),
+                  country_target = substr(target,1,3),
+                  item_target = substr(target,5,100))]
+    
+    results[,`:=`(group_origin = items$comm_group[match(results$item_origin,items$item)],
+                  group_target = items$comm_group[match(results$item_target,items$item)],
+                  continent_origin = regions$continent[match(results$country_origin, regions$iso3c)])]
+    
+    results$continent_origin[results$country_origin==country] <- country
+    
+    # Remove "Live animals" from the nutrient flows
+    # CHECK: Need to be in energy/labor footprints?
+    results = results %>%
+      filter(group_origin != "Live animals")
+    
+    # print(paste(country, "ratio of FD vs. production:", 
+    #             round(sum(as.matrix(Y_country)[, consumption]) / sum(results$value),4)))
+    
+    data_tot <- results %>%
+      group_by(item_target, country_origin) %>%
+      filter(value != 0) %>%
+      summarise(value = (sum(value))) %>%
+      spread(country_origin, value, fill = 0) %>% # Al
+      # Add a row with column sums
+      ungroup() %>%
+      bind_rows(summarise(., item_target = "Total", across(-item_target, sum)))
+    
+    data.table::fwrite(data_tot, file=paste0("output/FABIO_", country,"_", year, "_", extension, "_", consumption,".csv"), sep=",")
+    
+    data_imp_tot = tail(data_tot, 1) %>% rename(importer = item_target) %>% mutate(importer = country)
+    
+    # Fill mat with data_imp_tot where rownames(mat) match names(data_imp_tot), and colnames(mat) match data_imp_tot$importer
+    mat[rownames(mat) %in% names(data_imp_tot), data_imp_tot$importer[1]] <- 
+      as.numeric(data_imp_tot[1, names(data_imp_tot) %in% rownames(mat)])
+  }
+}
+
+# Total Mcal trade between countries
+saveRDS(mat, file.path("data/calorie_trade_mat.rds"))
+
+
+
+# TODO
+# - Consider per-capita results
+# - Consider which metric to present
+# - Consider how to handle energy & hour vs. calorie & protein
+
+
+
+
+
+# ====
+
 # Format the output matrices
 Y_cons = data.table(as.matrix(FABIO_y_hh_cal))
 # rownames(Y_cons) <- paste0(io$iso3c, "_", io$item)
