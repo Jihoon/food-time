bf4f8411b4633f19361f2837560898e4277ab2a1
Jihoon Min <grcosmos@gmail.com>
2026-03-30 16:58:44 +0200

    Add non-economic GHD data import

--- 0.mrio_prep.R
+++ 0.mrio_prep.R
@@ -1,9 +1,6 @@
-library(mrio)
 library(tidyverse)
 library(Matrix)
 
-# load some utility functions
-source("99.utils.R")
 
 # Key parameters for config
 year = yr = 2020  # Year when both labor and energy satellites are available
@@ -31,6 +28,7 @@ FABIO_x = readRDS(file.path(FABIO_path,"X.rds"))[,as.character(year)]
 FABIO_y = readRDS(file.path(FABIO_path,"Y.rds"))[[as.character(year)]]
 FABIO_L = readRDS(file.path(FABIO_path, paste0(year, "_L_value.rds")))
 FABIO_y_hh = FABIO_y[,grep("food", colnames(FABIO_y))] # 23001x187
+FABIO_x_hh = FABIO_L %*% FABIO_y_hh
 
 # Make FABIO-EXIOBASE region mapping 
 reg_map = readxl::read_xlsx("data/fabio-exiobase.xlsx", 
@@ -65,6 +63,8 @@ fd <- fread(file=file.path(FABIO_path,"fd_labels.csv"))
 n_col <- ifelse(type == "ixi", 7989, 9802)
 
 # Read EXIO energy and labor direct satellite accounts
+# Energy in TJ
+# Labor in M.hour
 ene = as.matrix(data.table::fread(EXIO_path_e, select = 2:(n_col-1), skip = 3,
                                   header = F))
 lab = as.matrix(data.table::fread(EXIO_path_l, select = 2:(n_col-1), skip = 3,
@@ -77,6 +77,7 @@ EXIO_Y = as.matrix(data.table::fread(paste0(EXIO_path, "/Y.txt"), skip = 3,
                                      select = 3:(7*49+2), header = F))
 EXIO_reg = data.table::fread(paste0(EXIO_path, "/unit.txt"), header = T)
 n_reg_EXIO = length(unique(EXIO_reg$region)) # = 49
+EXIO_sect = unique(EXIO_reg$sector)
 
 # Take the 'net energy'
 # Note: "A net energy use account includes the energy content of all energy products 
@@ -93,7 +94,7 @@ lab_female = matrix(colSums(lab[c(8,10,12),]))
 
 
 
-#### Prepare indirect satellite through Leontief inverse 'L' ####
+#### 3. Prepare/Read indirect satellite through Leontief inverse 'L' ####
 # DO ONLY ONCE AND SAVE THE RESULTING L MATRIX
 # 
 # # read matrices
@@ -108,3 +109,47 @@ lab_female = matrix(colSums(lab[c(8,10,12),]))
 
 # Load Leontief inverse L
 EXIO_L = readRDS(file = paste0("data/EXIO_L_", year, "_", type, ".rds"))
+
+
+#### 4. Read Fajzel data ####
+
+path_GHD = "H:/MyDocuments/Data/GlobalHumanDay/"
+
+# 0 Identify available country/year combinations in the data ####
+# Read file names from the inputData folder under path_GHD
+file_names = list.files(path = paste0(path_GHD, "inputData/"), pattern = "*.csv", full.names = TRUE)
+
+# Use regex to extract the country names from three letters coming after "inputData/" and before "_TUS"
+countries = str_extract(file_names, "(?<=inputData/)[A-Z]{3}(?=_TUS)")
+
+# Use regex to extract years from the integers in file_names containing "TUS"
+years = str_extract(file_names, "(?<=TUS_)[0-9]{4}")
+
+# 4.1. Read the raw country data ####
+# df_ghd = read_csv(paste0(path_GHD, "outputData/country_specific_results.csv"))
+df_ghd_gender = read_csv(paste0(path_GHD, "outputData/gender_split/TUS_only_M24_251017.csv")) %>%
+  filter(subcategory %in% c("processing", "preparation", "growth_collection")) %>%
+  mutate(subcategory = case_when(
+    subcategory == "processing" ~ "processing_non.econ",
+    subcategory == "preparation" ~ "preparation_non.econ",
+    subcategory == "growth_collection" ~ "growth_collection_non.econ"
+  )) 
+
+# Non-economic food time by gender
+df_ghd_gender = df_ghd_gender %>% rename(footprint_type = subcategory) %>%
+  # Pivot longer to have a column "type" with values "hr_m" and "hr_f", and a column "per_capita_value" with the corresponding hours
+  pivot_longer(cols = c("maleTotalHours", "femaleTotalHours"), names_to = "type", values_to = "per_capita_value") %>%
+  mutate(type = ifelse(type == "maleTotalHours", "hr_m", "hr_f")) %>% 
+  select(country, type, footprint_type, per_capita_value) %>%
+  drop_na() # Drop rows with NA values in per_capita_value
+
+# 4.2. Read the restaurant data ####
+df_restaurant = read_csv(paste0(path_GHD, "outputData/restaurants_accommodations_time.csv")) %>% 
+  select(country, hr_m = mean_hrs_restaurants, hr_f = mean_hrs_restaurants) %>%
+  # stack df_restaurant twice with type "hr_m" and "hr_f" to match the structure of df_ghd_gender
+  pivot_longer(cols = c(hr_m, hr_f), names_to = "type", values_to = "per_capita_value") %>%
+  mutate(footprint_type = "preparation_econ") %>%
+  select(country, type, footprint_type, per_capita_value)
+
+# 4.3. Combine the two dataframes ####
+df_ghd_combined = bind_rows(df_ghd_gender, df_restaurant)
